5367b5f2f2855d96d8deea0231ebeedfcea3e3bf
marc310k <marcelo.koskur@gmai.com>
2021-11-10 16:23:53 -0300

    deixando o codigo mmais clean

--- Aula 9/checkpoint.js
+++ Aula 9/checkpoint.js
@@ -1,12 +1,10 @@
 /*- Precisamos desenvolver um menu para um microondas super veloz, onde teremos
  5 opções de comida com seus respectivos tempos pré-definidos. 
-
       1 - Pipoca – 10 segundos (padrão);
       2 - Macarrão – 8 segundos (padrão);
       3 - Carne – 15 segundos (padrão);
       4 - Feijão – 12 segundos (padrão);
       5 - Brigadeiro – 8 segundos (padrão); 
-
 - O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo 
 com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir 
 mensagem que a comida queimou.
@@ -17,8 +15,8 @@ a mensagem: “kabumm”;
 - No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, 
 bom apetite!!!".*/
 
-//PIPOCA
 
+//PIPOCA
 function pipoca(tempoUsuario){
     if((tempoUsuario) > (2*tempoPadrao)){
         console.log('A comida queimou');
@@ -37,7 +35,6 @@ var tempoPadrao = 10;
 pipoca(5);
 
 //MACARRAO
-
 function macarrao(tempoUsuario){
     if((tempoUsuario) > (2*tempoPadrao)){
         console.log('A comida queimou');
@@ -56,7 +53,6 @@ var tempoPadrao = 8;
 macarrao(8);
 
 //CARNE
-
 function carne(tempoUsuario){
     if((tempoUsuario) > (2*tempoPadrao)){
         console.log('A comida queimou');
@@ -75,7 +71,6 @@ var tempoPadrao = 15;
 carne(31);
 
 //FEIJAO
-
 function feijao(tempoUsuario){
     if((tempoUsuario) > (2*tempoPadrao)){
         console.log('A comida queimou');
@@ -94,7 +89,6 @@ var tempoPadrao = 12;
 feijao(8);
 
 //BRIGADEIRO
-
 function brigadeiro(tempoUsuario){
     if((tempoUsuario) > (2*tempoPadrao)){
         console.log('A comida queimou');
