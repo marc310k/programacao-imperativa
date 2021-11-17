a6afbee837cc89e24aab7b2c8fc8d9c7820029fc
marc310k <marcelo.koskur@gmail.com>
2021-11-17 16:02:11 -0300

    aeeee caraio

---
+++ Aula 11/microdesafio.js
@@ -0,0 +1,41 @@
+//Repetir como um papagaio
+//Crie um loop utilizando for que se repita 5 vezes. Dentro de cada repetição se deve mostrar no console a mensagem “Olá mundo”.
+
+for (let repet = 1; repet <=5; repet++) {
+    console.log('Olá Mundo!');
+};
+
+//Contando números ímpares
+//Crie um loop for de 1 a 10, em que exiba no console apenas o valor das repetições de números ímpares. Dica: A ideia é que nas 
+//repetições ímpares de 0 ao 10 (1,3,5,7,9) sejam mostrados os números das repetições no console.
+
+for (let impares = 1; impares <=10; impares++) {
+    console.log(impares + (impares +1));
+};
+
+
+//Criando a tabuada
+//Crie uma tabuada utilizando o for. Dica: Para essa tarefa você irá precisar utilizar dois loops (for).*/
+
+//2
+for (let tabuada = 1; tabuada <=10; tabuada++) {
+    console.log((tabuada)*2);
+};
+
+//3
+for (let tabuada = 1; tabuada <=10; tabuada++) {
+    console.log((tabuada)*3);
+};
+
+//4
+for (let tabuada = 1; tabuada <=10; tabuada++) {
+    console.log((tabuada)*4);
+};
+
+//x
+for (let tabuada = 1; tabuada <=10; tabuada++) {
+    let x = 6;  // INFORME AQUI O NÚMERO DA TABUADA
+    console.log(x + ' x ' + (tabuada) + ' é igual a ' + (tabuada)*x);
+};
+
+// EU CONSEGUI!!! =D
