30ac785358c003806fa292d5cbd179a364d2b2f7
marc310k <marcelo.koskur@gmail.com>
2021-11-22 21:02:01 -0300

    json e objeto

---
+++ Aula 13/exercicios.js
@@ -0,0 +1,13 @@
+let contaBancaria = {
+    numeroConta: 1234,
+    tipoConta: "Conta Corrente",
+    //saldo: 5000000,
+    titularConta: "Marcelo Pacholak",
+    valorSaida: 50000,
+    valorEntrada: 100000,
+
+    calcularSaldo: function () {
+        return this.valorEntrada-this.valoa
+    }
+};
+
